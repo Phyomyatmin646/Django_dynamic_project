7b230a6539772cc5731a33d275b1a6c31aaf300f
Phyo Myat Min <phyomyatmin646@gmail.com>
2026-02-28 10:16:13 +0630

    s

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,6 @@ from .views import Homepage, download_cv
 from . import views
 
 urlpatterns = [
-    path('home/', views.Homepage, name='homepage'),
+    path('', views.Homepage, name='homepage'),
     path('download-cv/', download_cv, name='download_cv'),
 ]
