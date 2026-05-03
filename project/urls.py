5bdcf9426836065163816abdc5b27628f0840c9e
Phyo Myat Min <phyomyatmin646@gmail.com>
2026-05-03 10:15:28 +0630

    f

--- project/urls.py
+++ project/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ckeditor/', include('ckeditor_uploader.urls')),  # ← fixes the error
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('myapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
